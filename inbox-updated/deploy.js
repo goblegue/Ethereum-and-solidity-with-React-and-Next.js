db901fff3cfa3a78e978270cb43678ac4bd67ecf
goblegue <hmehboob.bese21seecs@seecs.edu.pk>
2024-01-16 16:21:25 +0500

    message: updated deployment of sol contract

--- inbox-updated/deploy.js
+++ inbox-updated/deploy.js
@@ -1,11 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const {
-    Web3
-} = require('web3');
-const {
-    interface,
-    bytecode
-} = require('./compile');
+const { Web3 } = require('web3');
+
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
     'rare march blossom reason inner say erode switch pitch verb staff twist',
@@ -15,21 +11,15 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
-
+  
     console.log('Attempting to deploy from account', accounts[0]);
-
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({
-            data: bytecode,
-            arguments: ['hello world']
-        })
-        .send({
-            from: accounts[0],
-            gas: '1000000'
-            
-        });
-
-    console.log('contract is deployed to the address', result.options.address);
+  
+    const result = await new web3.eth.Contract(abi)
+      .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+      .send({ gas: '1000000', from: accounts[0] });
+  
+    console.log('Contract deployed to', result.options.address);
     provider.engine.stop();
-}
+  };
+  
 deploy();
