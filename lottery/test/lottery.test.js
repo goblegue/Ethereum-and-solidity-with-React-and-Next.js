2c3ffc26ad811478880433ea222a3f27958a010d
goblegue <hmehboob.bese21seecs@seecs.edu.pk>
2024-01-17 18:52:40 +0500

    test setup

--- lottery/test/lottery.test.js
+++ lottery/test/lottery.test.js
@@ -34,5 +34,76 @@ describe("Lottery", () => {
 
     assert.equal(accounts[0], players[0]);
     assert.equal(1,players.length);
+  });
+  it('multiple players can enter ',async ()=>{
+
+    await lottery.methods.enter().send({ 
+      from : accounts[0],
+      value : web3.utils.toWei("0.02","ether")
+    });
+    await lottery.methods.enter().send({ 
+      from : accounts[1],
+      value : web3.utils.toWei("0.02","ether")
+    });
+    await lottery.methods.enter().send({ 
+      from : accounts[2],
+      value : web3.utils.toWei("0.02","ether")
+    });
+    const players= await lottery.methods.allPlayers().call();
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3,players.length);
+  });
+  it("require minimum amount amount of ether to enter",async () => {
+    try{
+      await lottery.methods.enter().send({
+        from : accounts[0],
+        value : 0
+      });
+      assert(false);
+    }
+    
+    catch(err){
+      assert(err);
+    }
+  });
+  it("manager can only pick Winner", async () => {
+    try{
+      await lottery.methods.pickWinner().send({
+        from : accounts[1],
+      });
+      assert(false);
+    }
+    catch(err){
+      assert(err);
+    }
   })
+  it('multiple players can enter ',async ()=>{
+
+    await lottery.methods.enter().send({ 
+      from : accounts[0],
+      value : web3.utils.toWei("0.02","ether")
+    });
+    await lottery.methods.enter().send({ 
+      from : accounts[1],
+      value : web3.utils.toWei("0.02","ether")
+    });
+    await lottery.methods.enter().send({ 
+      from : accounts[2],
+      value : web3.utils.toWei("0.02","ether")
+    });
+    await lottery.methods.pickWinner().send({
+      from : accounts[0],
+    });
+    winners = 
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3,players.length);
+  });
+
+
 });
